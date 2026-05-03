c98a4a4b29a72a152092c6991158008238357375
Sanya Chetwani <ssanyachetwani@gmail.com>
2026-05-02 20:16:20 -0400

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,37 @@
+class Node:
+
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            # to do
+            if char not in curr.children:
+                curr.children[char] = Node()
+            curr = curr.children[char]
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True 
+        
+        
